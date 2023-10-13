eb59ea62f1c509f3885a3dd104134b5a2c9ce766
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-10-13 12:00:20 +0100

    Define basic stack operations

--- Stack Operations /Stack-implementation.c
+++ Stack Operations /Stack-implementation.c
@@ -1,1 +1,9 @@
 // Implements a Last In First Out Data Structure in C 
+//### Operations: 
+#include<stdio.h>
+void operations()
+{
+    printf("1. Push (place) an Element into a stack. \n");
+    printf("2. Pop an Element into a stack. \n");
+    printf("3. Peek. (To return the first element.)  \n");
+}
