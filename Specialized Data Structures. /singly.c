6d03694143662db105766e733c9bbc8b85e5a349
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2024-01-16 14:40:01 +0100

    Create nodes, insert node at begining

--- Specialized Data Structures. /singly.c
+++ Specialized Data Structures. /singly.c
@@ -9,6 +9,7 @@ struct slinklist{
     struct slinklist* next;
 };
 typedef struct slinklist node;
+node* start = NULL;
 node* getnode()
 {
     node* newnode = (node*) malloc(sizeof(node));
@@ -17,3 +18,33 @@ node* getnode()
     newnode->next= NULL;
     return newnode;
 }
+// Creating a singly with 'n' number of nodes
+void createlist(int n){
+    int i; 
+    node* newnode;
+    node* temp;
+    for(i=0;i<n;i++)
+    {
+        if(start==NULL){
+            start =  newnode;
+        }
+        else{
+            temp = start;
+            while(temp-> next!=NULL)
+            temp =  temp -> next;
+            temp -> next= newnode;
+        }
+    }
+}
+//Insert node at the beginning of the list
+void inserbeg(int n){
+    node* newnode;
+    newnode = getnode();
+    if(start == NULL){
+        start = newnode;
+    }
+    else{
+        newnode->next = start;
+        start = newnode;
+    }
+    } 
